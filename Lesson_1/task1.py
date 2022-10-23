e52b1ea39dff0a1b9a20014e17a1ff3305305ec5
didislava <15vladushka@gmail.com>
2022-10-23 23:20:13 +0300

    add task 1

---
+++ Lesson_1/task1.py
@@ -0,0 +1,25 @@
+﻿import math
+print("for using √ enter // as operation and 1 as second number")
+print("for using ^ enter 2 as second number")
+first_number = int(input("enter 1st number "))
+operation = input("operation (+,-,*,/) ")
+second_number = int(input("enter 2nd number "))
+
+
+if operation == "+":
+    print("result = " + str(first_number+second_number))
+
+if operation == "-":
+    print("result = " + str(first_number-second_number))
+
+if operation == "*":
+    print("result = " + str(first_number*second_number))
+
+if operation == "/":
+    print("result = " + str(first_number/second_number))
+
+if operation == "//":
+    print("result = " + str(math.sqrt(first_number)))
+
+if operation == "^":
+    print("result = " + str(first_number*first_number))
