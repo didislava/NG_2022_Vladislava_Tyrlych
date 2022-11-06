080e47aa06d14418c4af94bac7d75629acb016cd
didislava <15vladushka@gmail.com>
2022-11-06 21:48:14 +0200

    add 4 task

---
+++ Lesson_1/task4.py
@@ -0,0 +1,19 @@
+﻿﻿import math
+a = float(input("please, enter a:"))
+b = float(input("please, enter b:"))
+c = float(input("please, enter c:"))
+
+if a == 0:
+    print("a can not be 0")
+else:
+    discriminant = (b*b)-4*a*c
+
+if discriminant < 0:
+    print("There are no real roots")
+if discriminant == 0:
+    x = (-b/(2*a))
+    print("x = " + x)
+if discriminant > 0:
+    fx = ((-b+(math.sqrt(discriminant)))/(2*a))
+    sx = ((-b-(math.sqrt(discriminant)))/(2*a))
+    print("There are two roots: " + fx + "and " + sx)
